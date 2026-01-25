42db0f72aa04189cee0b9d0afad8206ba082b91d
aaaaarvi <arvi.jj@gmail.com>
2026-01-25 17:42:59 +0100

    update vector class

--- include/vector.h
+++ include/vector.h
@@ -1,11 +1,47 @@
 #pragma once
 
-#include <cstdlib>
-#include <vector>
-
-struct Vector2D {
-    double x, y;
-    Vector2D();
-    Vector2D(double x, double y);
-    ~Vector2D();
+template<typename T>
+class Vector2 {
+public:
+    T x;
+    T y;
+    Vector2<T>(T X = 0, T Y = 0) : x(X), y(Y) {}
+    ~Vector2() {}
+    Vector2<T> operator+(const Vector2<T>& other) const {
+        return Vector2<T>(x + other.x, y + other.y);
+    }
+    Vector2<T> operator-(const Vector2<T>& other) const {
+        return Vector2<T>(x - other.x, y - other.y);
+    }
+    Vector2<T> operator*(T scalar) const {
+        return Vector2<T>(x * scalar, y * scalar);
+    }
+    Vector2<T> operator/(T scalar) const {
+        return Vector2<T>(x / scalar, y / scalar);
+    }
+    Vector2<T>& operator+=(const Vector2<T>& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+    Vector2<T>& operator-=(const Vector2<T>& other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+    Vector2<T>& operator*=(const Vector2<T>& other) {
+        x *= other.x;
+        y *= other.y;
+        return *this;
+    }
+    Vector2<T>& operator/=(const Vector2<T>& other) {
+        x /= other.x;
+        y /= other.y;
+        return *this;
+    }
 };
+
+typedef Vector2<float> Vector2f;
+typedef Vector2<double> Vector2d;
+typedef Vector2<int> Vector2i;
+typedef Vector2<unsigned int> Vector2u;
